336e68d5ba33a3c1625984625fc70f7d30f328d4
Ash <65588168+ashpotter@users.noreply.github.com>
2021-01-12 17:28:09 +0300

    reply.js 2.0.0 sürümüne güncellendi ngl
    OBJECTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT

--- reply.js
+++ reply.js
@@ -1,52 +1,94 @@
 /* 
 REPLY MANAGER BY POKETALE 
-1.3.1 (c) 2021 Poketale bot
+2.0.0 (c) 2020-2021 Poketale bot
+ÇALINMASI KESİNLİKLE YASAKTIR
 LİSANS:MİT
 */
 
+
+
+
+
+
 class ReplyManeger {
+static istrue(d){
+  var e = d
+  if (e = true) {
+  var b = e
+  return b
+}
+  if (e = false) {
+  var b = e
+  return b
+}
+}
       static reply(msg, d,error){ 
     console.error("[REPLY UTİL] DEPRECATED:.reply yerine lütfen .reply.true yada .reply.false kullanın")
-        return "[REPLY UTİL] DEPRECATED:.reply yerine lütfen .reply.true yada .reply.false kullanın"
+         throw new TypeError  ("[REPLY UTİL] DEPRECATED:.reply yerine lütfen .reply.true yada .reply.false kullanın")
     }
-    static true(msg, d){ //etiket atan reply
+    static true(msg, d){ 
         var e = true
-    if(!msg) return `.reply(message,"mesajın") şeklinde yazmayı unutma`
+    if(!msg) throw new TypeError (`.true(message,"mesajın") şeklinde yazmayı unutma`)
 
-  
-if(!d) return "Bir mesaj gir"
+  var msge = ({data:{
+    içerik:d,
+   replied_user:e,
+    message_id:msg.id,
+    kanal:msg.channel,
+    mesaj:msg
+  }});
+      
+if(!d) throw new TypeError ("Bir mesaj gir")
     msg.client.api.channels(msg.channel.id).messages.post({ 
     data: {
         content: d,   message_reference: { message_id: msg.id },
         allowed_mentions: { replied_user: e }
 }
  }).then(x => x.referenced_message);
-      return "Yay! Başardın!"
+      return msge
       console.log(`[REPLY UTİL] mesaj:${d}`)
     
 }
-static false(msg, d) { //etiket atmayan reply
+static false(msg, d) {
     var e = false
-    if(!msg) return `.reply(message,"mesajın") şeklinde yazmayı unutma`
+    if(!msg) throw new TypeError (`.false(message,"mesajın") şeklinde yazmayı unutma`)
 
   
-if(!d) return "Bir mesaj gir"
+if(!d) throw new TypeError ("Bir mesaj gir")
+  
+  var msge = ({data:{
+    içerik:d,
+    replied_user:e,
+    message_id:msg.id,
+    kanal:msg.channel,
+    mesaj:msg
+  }});
+  
     msg.client.api.channels(msg.channel.id).messages.post({ 
     data: {
         content: d,   message_reference: { message_id: msg.id },
         allowed_mentions: { replied_user: e }
 }
  }).then(x => x.referenced_message);
-        return "Yay! Başardın!"
+        return msge
           console.log(`[REPLY UTİL] mesaj:${d}`)
 
 }
+  
 }
-
-
-ReplyManeger.version = "1.3.1"
-
-
-
+ var msge = {
+true:ReplyManeger.true,
+false:ReplyManeger.false
+  }
+ 
+ 
+ ReplyManeger.version = "2.0"
+ 
 module.exports = ReplyManeger;
 
+
+/*
+2.0.0 YENİLİKLER - REPLY MANAGER BY POKETALE BOT
+-Tüm return hataları throw new TypeError ile değiştirildi
+-reply.<true/false>.data eklendi
+*/
